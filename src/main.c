e352e014abd4dcace246f40fe8cd255cc6f69ebb
Evan Rittenhouse <evanrittenhouse@gmail.com>
2024-10-20 21:36:30 -0500

    bind()

--- src/main.c
+++ src/main.c
@@ -1,25 +1,55 @@
-#include <netdb.h>
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <errno.h>
+ #include <string.h>
+ #include <sys/types.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <netdb.h>
+ #include <arpa/inet.h>
+ #include <sys/wait.h>
+ #include <signal.h>
 
 const char *PORT = "5757";
 
 const int CRV_OK = 1;
 const int CRV_ERR = -1;
 
-int crv_getaddrinfo(struct addrinfo *hints, struct addrinfo **res) {
-   int get_addrinfo_status;
+int crv_get_socket(struct addrinfo hints, struct addrinfo *res, int *socket_fd) {
+   struct addrinfo *p;
+   int get_addrinfo_status, socket_res;
    
-   // Fills `res` out with a linked-list of `addrinfo`s, which contains `sockaddr`s that we can use later
-   get_addrinfo_status = getaddrinfo(NULL, PORT, hints, res);
+   // Fills `res` with a linked-list of `addrinfo`s, which contains `sockaddr`s that we can use later
+   get_addrinfo_status = getaddrinfo(NULL, PORT, &hints, &res);
    if (get_addrinfo_status == -1) {
       fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(get_addrinfo_status));
       return CRV_ERR;
    }
+
+   // Loop through all returned `addrinfo`s and bind to the first one we can 
+   for (p = res; p != NULL; p = p->ai_next) {
+      socket_res = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+      if (socket_res == -1) {
+         socket_res = errno;
+         fprintf(stderr, "socket error: %d\n", socket_res);
+         continue;
+      }
+
+      // Associate a socket with an IP address and port nmber
+      if (bind(socket_res, p->ai_addr, p->ai_addrlen) == -1) {
+         close(socket_res);
+         fprintf(stderr, "bind error");
+         continue;
+      }
+
+      break;
+   }
+
+   *socket_fd = socket_res;
+
+   freeaddrinfo(res);
 
    return CRV_OK;
 }
@@ -27,6 +57,7 @@ int crv_getaddrinfo(struct addrinfo *hints, struct addrinfo **res) {
 int main() {
    struct addrinfo hints;
    struct addrinfo *serv_info;
+   int socket_fd, socket_res;
 
    // Ensure hints is 0'd
    memset(&hints, 0, sizeof hints); 
@@ -37,11 +68,17 @@ int main() {
    // Sets IP address of returned sockaddr to INADDR_ANY
    hints.ai_flags = AI_PASSIVE;
 
-   printf("Hello, World!");
-
-   if (crv_getaddrinfo(&hints, &serv_info) == CRV_ERR) {
+   socket_res = crv_get_socket(hints, serv_info, &socket_fd);
+   if (socket_res == -1) {
       exit(1);
    }
+
+   if (listen(socket_fd, 10) == -1) {
+      printf("listen");
+      exit(1);
+   }
+
+   printf("socket: %d", socket_fd);
 
 
    // getaddrinfo to find a socket
